669d678b802859f531d4e46a2968e8f6f594b042
Ammaar <rasheedammaar@gmail.com>
2025-03-11 23:48:53 +0400

    add the html views and used templ with go rendering

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: false, // Disable purging if using dynamic content
+  content: [
+    './**/*.{templ,html}',
+    '*.{templ,html}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
